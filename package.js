2d1d21dec2aa0a3849c916109e90224d6fc74402
Joshua Ohlman <codewriterohs@gmail.com>
2014-10-15 09:00:32 -0500

    Publish v1.0.0

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  name: 'mailgun-emails',
-  summary: ' /* Fill me in! */ ',
+  name: 'cwohlman:mailgun-emails',
+  summary: 'Send and recieve emails via mailgun',
   version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  git: 'git@github.com:cwohlman/meteor-mailgun-emails.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.3');
   
-  api.use('cwohlman:emails');
+  api.use('cwohlman:emails@0.2.0');
   api.use('http');
   api.use('sha');
-  api.use('iron:router');
+  api.use('iron:router@0.9.3');
 
   api.addFiles('mailgun-emails.js');
 
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('mailgun-emails');
+  api.use('cwohlman:mailgun-emails');
   api.use('iron:router');
   api.addFiles('mailgun-emails-tests-key.js');
   api.addFiles('mailgun-emails-tests.js');
