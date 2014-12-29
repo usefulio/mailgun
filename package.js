c005e851fb7e590c61095cdbdadd55113869947e
Ian Serlin <ian@ianserlin.com>
2014-12-28 23:10:20 -0800

    cleanup

--- package.js
+++ package.js
@@ -6,14 +6,14 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('METEOR@1.0');
   
   api.use('cwohlman:emails@0.3.0');
   api.imply('cwohlman:emails');
 
   api.use('http');
   api.use('sha');
-  api.use('iron:router');
+  api.use('iron:router@1.0.0');
 
   api.addFiles('mailgun-emails.js');
 
@@ -22,7 +22,7 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('cwohlman:mailgun-emails');
+  api.use('useful:email-mailgun');
   api.use('iron:router');
   api.addFiles('mailgun-emails-tests.js');
 });
